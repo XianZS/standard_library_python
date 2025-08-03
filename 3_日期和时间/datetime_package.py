4889cc48d93c8f3bfa9e295e25712c01370301f4
XianZS <XianZhiSen_yang@outlook.com>
2025-08-04 00:15:30 +0800

    datetime

---
+++ 3_日期和时间/datetime_package.py
@@ -0,0 +1,23 @@
+"""
+    datetime 模块
+"""
+import datetime
+
+# 1. 获取当前时间和日期
+
+# 2. 创建特定日期和时间对象
+
+# 3. 解析字符串时间
+
+# 4. 时间的加减法
+
+# 5. 提取年月日 时分秒
+some=datetime.datetime.now()
+print(some.day)
+
+# 6. 替换日期时间部分
+
+
+
+
+
